b036ae6341c15897ca39d1e3158c772a35090742
Leonie Mortensvik <71489936+Leonim17@users.noreply.github.com>
2020-11-08 18:47:54 +0100

    Update init.sql

--- justnoted/db/init.sql
+++ justnoted/db/init.sql
@@ -1,7 +1,33 @@
 CREATE TABLE users
 (
-   id serial primary key, 
-   name varchar(255) not null,
-   email varchar(255) not null
-
+   id serial, 
+   user_name varchar(255) not null,
+   user_email varchar(255) not null,
+   user_active varchar(10) not null,
+   user_created varchar(255) not null,
+   PRIMARY KEY (id)
 );
+
+CREATE TABLE lists
+(
+   id serial,
+   user_id serial,
+   list_name varchar(255) not null,
+   list_created varchar(255) not null,
+   list_active varchar(255) not null,
+   PRIMARY KEY (id),
+   FOREIGN KEY (user_id) REFERENCES user (id)
+);
+
+CREATE TABLE tasks
+(
+   id serial,
+   user_id serial,
+   list_id serial,
+   task_description varchar(255) not null,
+   task_created varchar(255) not null
+   PRIMARY KEY (id),
+   FOREIGN KEY (user_id) REFERENCES users (id),
+   FOREIGN KEY (list_id) REFERENCES lists (id)
+);
+
